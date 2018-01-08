1b59e8e5445b9fe49a39924e494cc543406090b8
Unknown <esquinca_jose@hotmail.com>
2018-01-08 09:03:41 -0500

    Tabla Equipos
    Seccion de equipos en geolocation.

--- public/js/admin/geolocation.js
+++ public/js/admin/geolocation.js
@@ -10,7 +10,7 @@ $(function() {
 function init() { 
   moment.locale('es'); 
  
-    map = new google.maps.Map(document.getElementById('googlemap'), { 
+  map = new google.maps.Map(document.getElementById('googlemap'), { 
     zoom: 8, 
     center: new google.maps.LatLng(20.960072,-87.264404), 
     //mapTypeId: google.maps.MapTypeId.ROADMAP 
@@ -19,7 +19,45 @@ function init() {
   infowindow = new google.maps.InfoWindow; 
  
   addLocation(); 
-} 
+}
+
+var configTableAps={
+      "order": [[ 0, "desc" ]],
+      paging: true,
+      //"pagingType": "simple",
+      Filter: true,
+      searching: true,
+      //"aLengthMenu": [[5, 10, 25, -1], [5, 10, 25, "All"]],
+      ordering: true,
+      //"pageLength": 5,
+      bInfo: false,
+      responsive: true,
+      language:{
+              "sProcessing":     "Procesando...",
+              "sLengthMenu":     "Mostrar _MENU_ registros",
+              "sZeroRecords":    "No se encontraron resultados",
+              "sEmptyTable":     "Ningún dato disponible",
+              "sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+              "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
+              "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+              "sInfoPostFix":    "",
+              "sSearch":         "Buscar:",
+              "sUrl":            "",
+              "sInfoThousands":  ",",
+            "sLoadingRecords": "Cargando...",
+              "oPaginate": {
+                "sFirst":    "Primero",
+                "sLast":     "Último",
+                "sNext":     "Siguiente",
+                "sPrevious": "Anterior"
+            },
+            "oAria": {
+                  "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+                  "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+              }
+      }
+}
+
  
 function addLocation(){ 
   let something; 
@@ -37,55 +75,81 @@ function addLocation(){
   $.ajax({ 
     url: "/geoLoc", 
     type: "GET", 
-    success: function (data) { 
-      //console.log(data); 
+    success: function (data) {
       datax = JSON.parse(data); 
-          console.log(datax); 
+      console.log(datax); 
       //something = datax.devices.device[0].gps_fix["@attributes"].latitude; 
-          //$datax = $decode->devices->device[0]->gps_fix->{'@attributes'}; 
-          count =  datax.devices.device; 
- 
-          //console.log(Object.keys(count)); 
-          count = Object.keys(count).length -2; 
-          //algo = datax.devices.device.length; 
-          //console.log(count); 
-          //console.log(algo); 
-          // lat = datax.devices.device[7]["@attributes"].name; 
-          // console.log(lat); 
- 
-          for (i = 0; i < count; i++) { 
-            //console.log("iteración: " + i); 
-            if (datax.devices.device[i].hasOwnProperty(myProp)) { 
-              status = "Online"; 
-              object1.push( 
-                { 
-                  name: datax.devices.device[i]["@attributes"].name, 
-                  mac: datax.devices.device[i]["@attributes"].ethernet_mac, 
-                  last_contact: datax.devices.device[i]["@attributes"].last_contact, 
-                  heading: datax.devices.device[i].gps_fix["@attributes"].heading, 
-                  lat: datax.devices.device[i].gps_fix["@attributes"].latitude, 
-                  lng: datax.devices.device[i].gps_fix["@attributes"].longitude, 
-                  speed: datax.devices.device[i].gps_fix["@attributes"].speed, 
-                  time: datax.devices.device[i].gps_fix["@attributes"].time, 
-                  status: status 
-                } 
-              ); 
-            }else{ 
-              status = "Offline"; 
-              object1.push( 
-                { 
-                  name: datax.devices.device[i]["@attributes"].name, 
-                  mac: datax.devices.device[i]["@attributes"].ethernet_mac, 
-                  last_contact: datax.devices.device[i]["@attributes"].last_contact, 
-                  status: status 
-                } 
-              ); 
-             } 
-          } 
-          console.log(object1); 
+      //$datax = $decode->devices->device[0]->gps_fix->{'@attributes'}; 
+      count =  datax.devices.device; 
+
+      //console.log(Object.keys(count)); 
+      count = Object.keys(count).length -2; 
+      //algo = datax.devices.device.length; 
+      //console.log(count); 
+      //console.log(algo); 
+      // lat = datax.devices.device[7]["@attributes"].name; 
+      // console.log(lat); 
+
+      $('#tableDatosGPS').DataTable().destroy();
+      var TablaGPS= $('#tableDatosGPS').dataTable(configTableAps);
+
+      // TablaGPS.fnAddData([
+      //   dataCali.Vertical,dataCali.Sitios,datames6,datames5,datames4,datames3,datames2,datames1,
+      // ]);
+
+      for (i = 0; i < count; i++) { 
+        //console.log("iteración: " + i);
+        last_contact = datax.devices.device[i]["@attributes"].last_contact;
+        last_contact = moment.unix(last_contact).format("YYYY-MM-DD HH:mm:ss");
+
+        if (datax.devices.device[i].hasOwnProperty(myProp)) {
+          status = "Online";
+          object1.push(
+            {
+              name: datax.devices.device[i]["@attributes"].name,
+              mac: datax.devices.device[i]["@attributes"].ethernet_mac,
+              last_contact: datax.devices.device[i]["@attributes"].last_contact,
+              heading: datax.devices.device[i].gps_fix["@attributes"].heading,
+              lat: datax.devices.device[i].gps_fix["@attributes"].latitude,
+              lng: datax.devices.device[i].gps_fix["@attributes"].longitude,
+              speed: datax.devices.device[i].gps_fix["@attributes"].speed,
+              time: datax.devices.device[i].gps_fix["@attributes"].time,
+              status: status
+            } 
+          );
+          TablaGPS.fnAddData([
+            datax.devices.device[i]["@attributes"].name,
+            datax.devices.device[i]["@attributes"].ethernet_mac,
+            datax.devices.device[i].gps_fix["@attributes"].latitude,
+            datax.devices.device[i].gps_fix["@attributes"].longitude,
+            datax.devices.device[i].gps_fix["@attributes"].speed,
+            last_contact,
+            status
+          ]);
+        }else{ 
+          status = "Offline"; 
+          object1.push( 
+            { 
+              name: datax.devices.device[i]["@attributes"].name, 
+              mac: datax.devices.device[i]["@attributes"].ethernet_mac, 
+              last_contact: datax.devices.device[i]["@attributes"].last_contact, 
+              status: status 
+            } 
+          );
+          TablaGPS.fnAddData([
+            datax.devices.device[i]["@attributes"].name,
+            datax.devices.device[i]["@attributes"].ethernet_mac,
+            0.00,
+            0.00,
+            0.00,
+            last_contact,
+            status
+          ]); 
+         } 
+      } 
+      //console.log(object1); 
  
       var j; 
- 
  
       for (j = 0; j < object1.length; j++) { 
         if (object1[j].status === "Online") { 
@@ -103,9 +167,7 @@ function addLocation(){
               infowindow.open(map, marker); 
             } 
             })(marker, j)); 
-        }else{ 
- 
-        } 
+        }
       } 
  
     }, 
@@ -133,7 +195,9 @@ function deleteMarkers() {
 function refreshMarkers() { 
   deleteMarkers(); 
   addLocation(); 
-} 
+}
+
+
  
 $('#btn-refresh').click(function(){ 
   refreshMarkers() 
--- resources/views/permitted/geolocation.blade.php
+++ resources/views/permitted/geolocation.blade.php
@@ -35,6 +35,7 @@
             <h4 style="background-color:#f7f7f7; font-size: 18px; text-align: center; padding: 7px 10px; margin-top: 0;">
                 Ubicación de autobuses en tiempo real.
             </h4>
+
             <div class="media">
                 <div class="media-body">
                   <div class="row">
@@ -60,11 +61,54 @@
                 </div>
             </div>
 
-           </div>
+          </div>
         </div>
       </div>
     </div>
   </div>
+
+
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12 col-lg-12 col-sm-12 col-xs-12">
+        <div class="box box-solid">
+          <div class="box-body">
+            <h4 style="background-color:#f7f7f7; font-size: 18px; text-align: center; padding: 7px 10px; margin-top: 0;">
+                Tabla de equipos.
+            </h4>
+            
+            <div class="media">
+                <div class="media-body">
+                  <div class="row">
+                    <div class="col-md-12 col-lg-12 col-sm-12 col-xs-12">
+                      <div class="clearfix">
+                        <table id="tableDatosGPS" name="tableDatosGPS" class="table table-bordered table-striped" style="font-size: 85%;">
+                          <thead>
+                            <tr >
+                              <th class="bg-primary">Equipo</th>
+                              <th class="bg-primary">MAC</th>
+                              <th class="bg-primary">Latitud</th>
+                              <th class="bg-primary">Longitud</th>
+                              <th class="bg-primary">Velocidad</th>
+                              <th class="bg-primary">Último Contacto</th>
+                              <th class="bg-primary">Estado</th>
+                            </tr>
+                          </thead>
+                        </table>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
 @else
   @include('default.denied')
 @endif
